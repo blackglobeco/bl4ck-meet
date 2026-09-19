ed92df7f50356b67aeafab06edf001709d18b0c3
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2026-09-20 02:45:49 +0800

    Update firebase.js

--- src/server/firebase.js
+++ src/server/firebase.js
@@ -11,7 +11,7 @@ export const db = firebase;
 
 var firepadRef = firebase.database().ref();
 
-export const userName = prompt("What's your name?");
+export const userName = prompt("Enter your username:");
 const urlparams = new URLSearchParams(window.location.search);
 const roomId = urlparams.get("id");
 
